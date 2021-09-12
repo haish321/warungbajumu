1aaa993f6a09ccd7307b19e8190db0d1465b69a5
hadipsy27 <hadipsy27@gmail.com>
2021-09-12 10:06:02 -0700

    view & connect mongoDB

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var logger = require('morgan'); // use this to connect databases mongodb
+const mongoose = require("mongoose");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+mongoose.connect("mongodb://localhost:27017/db_warungbajumu",{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
